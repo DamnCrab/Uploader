33928b70f5bc459cd6499bc5a9f943c41085cb6d
DamnCrab <enormouscrab@gmail.com>
2025-03-10 15:51:52 +0800

    更换rollup

---
+++ rollup.config.js
@@ -0,0 +1,70 @@
+import pkg from './package.json';
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+
+const name = 'uploader';
+const NAME = name.charAt(0).toUpperCase() + name.substr(1);
+
+const banner = `/*!
+ * ${NAME} - ${pkg.description}
+ * @version v${pkg.version}
+ * @author ${pkg.author}
+ * @link ${pkg.homepage}
+ * @license ${pkg.license}
+ */`;
+
+export default [
+  // UMD build (unminified)
+  {
+    input: 'src/uploader.js',
+    output: {
+      file: 'dist/uploader.js',
+      format: 'umd',
+      name: 'Uploader',
+      banner
+    },
+    plugins: [
+      replace({
+        preventAssignment: true,
+        '__VERSION__': pkg.version
+      }),
+      nodeResolve(),
+      commonjs(),
+      babel({
+        babelHelpers: 'bundled',
+        presets: [['@babel/preset-env', { targets: { ie: '10' } }]]
+      })
+    ]
+  },
+  // UMD build (minified)
+  {
+    input: 'src/uploader.js',
+    output: {
+      file: 'dist/uploader.min.js',
+      format: 'umd',
+      name: 'Uploader',
+      banner,
+      sourcemap: true
+    },
+    plugins: [
+      replace({
+        preventAssignment: true,
+        '__VERSION__': pkg.version
+      }),
+      nodeResolve(),
+      commonjs(),
+      babel({
+        babelHelpers: 'bundled',
+        presets: [['@babel/preset-env', { targets: { ie: '10' } }]]
+      }),
+      terser({
+        output: {
+          comments: /^!/
+        }
+      })
+    ]
+  }
+];
